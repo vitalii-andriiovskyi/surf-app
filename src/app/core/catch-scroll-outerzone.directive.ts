0242d1e031c24ebf8dd2200cf41c34d48b84bccc
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-27 17:44:33 +0300

    correct comment for CatchScrollOuterzoneDirective

--- src/app/core/catch-scroll-outerzone.directive.ts
+++ src/app/core/catch-scroll-outerzone.directive.ts
@@ -2,7 +2,7 @@ import { Directive, NgZone, ElementRef, Renderer2 } from '@angular/core';
 
 import { PassScrollingDataService } from './pass-scrolling-data.service';
 
-// this directive attaches scroll event to .main-surf-wrapper (I set height html, body as 100%
+// this directive attaches scroll event to .main-surf-wrapper (I set height of <html> and <body> as 100%
 // and made wrapper be scrollable ) outside AngularZone and pass data (object UIEvent) using Observable
 // it helps to minimize Change Detection calls and fasten app during scrolling
 @Directive({

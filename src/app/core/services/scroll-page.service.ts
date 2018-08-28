2bedc73468343793fbb662be538c42c855e90616
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-28 13:03:11 +0300

    add methods msElapsed() and duration(); ScrollPageService

--- src/app/core/services/scroll-page.service.ts
+++ src/app/core/services/scroll-page.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { defer, animationFrameScheduler, interval } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,22 @@ import { Injectable } from '@angular/core';
 export class ScrollPageService {
 
   constructor() { }
+
+  msElapsed(sceduler = animationFrameScheduler) {
+    return defer(() => {
+      const start = sceduler.now();
+
+      return interval(0, animationFrameScheduler).pipe(
+        map(() => sceduler.now() - start)
+      );
+    });
+  }
+
+  duration(ms, sceduler = animationFrameScheduler) {
+    return this.msElapsed(sceduler).pipe(
+      map(ems => ems / ms),
+      takeWhile(t => t <= 1)
+    );
+  }
+
 }

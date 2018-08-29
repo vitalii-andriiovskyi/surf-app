0a87ff2892741a0e7649d842c9a1c0085944ed98
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-29 14:36:47 +0300

    fix "error TS2503: Cannot find namespace 'YT' testing"

--- src/app/surf-home/surf-section-about-us/surf-section-about-us.component.spec.ts
+++ src/app/surf-home/surf-section-about-us/surf-section-about-us.component.spec.ts
@@ -2,7 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, ActivatedRouteStub, Router, RouterStub, RouterLinkStubDirective } from '../../../testing/router-stubs';
 
 import { SurfSectionAboutUsComponent } from './surf-section-about-us.component';
-import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { NO_ERRORS_SCHEMA, NgModule, Component, Input, Output, EventEmitter } from '@angular/core';
+import { YoutubePlayerModule as YoutubePlayerModuleI } from 'ngx-youtube-player';
 
 let activatedRoute: ActivatedRouteStub;
 
@@ -12,8 +13,9 @@ describe('SurfSectionAboutUsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [YoutubePlayerModule],
       declarations: [ SurfSectionAboutUsComponent, RouterLinkStubDirective ],
-      schemas: [ NO_ERRORS_SCHEMA ],
+      // schemas: [ NO_ERRORS_SCHEMA ],
       providers: [
         {provide: Router, useValue: RouterStub },
         { provide: ActivatedRoute, useValue: activatedRoute },
@@ -32,3 +34,24 @@ describe('SurfSectionAboutUsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'youtube-player',
+  template: `<div>{{videoId}}</div>`
+})
+class YoutubePlayerComponent {
+  @Input() videoId: string;
+  @Input() height: string;
+  @Input() width: string;
+  @Input() protocol: string;
+  @Input() playerVars: string;
+  @Output() ready: EventEmitter<YT.Player> = new EventEmitter();
+  @Output() change: EventEmitter<YT.Player> = new EventEmitter();
+  constructor() {}
+}
+
+@NgModule({
+  declarations: [ YoutubePlayerComponent ],
+exports: [ YoutubePlayerComponent ]
+})
+class YoutubePlayerModule extends YoutubePlayerModuleI {}

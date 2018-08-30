f60e387a2156316d912bf91f1513e418493169c4
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-30 19:16:04 +0300

    fix issue 'collection.ensureIndex is deprecated'

--- src-server/libs/mongoose.ts
+++ src-server/libs/mongoose.ts
@@ -4,4 +4,5 @@ import { Connect } from 'mongoose';
 const config = require('/src-server/config');
 
 mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));
+mongoose.set('useCreateIndex', true);
 module.exports = mongoose;

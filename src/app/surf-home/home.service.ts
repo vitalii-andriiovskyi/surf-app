b55d6247d16785ec403d60b7c0f1185e1edf63c3
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-25 21:20:40 +0300

    inject 'ManagerTransferState' and change   getBoardsData(); HomeService

--- src/app/surf-home/home.service.ts
+++ src/app/surf-home/home.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AjaxApiService } from '../core/services/ajax-api.service';
+import { ManageTransferStateService } from '../core/services/manage-transfer-state.service';
+import { BoardData } from './board-data';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +9,15 @@ import { AjaxApiService } from '../core/services/ajax-api.service';
 export class HomeService {
   boardsToken = 'boards-';
 
-  constructor(private ajaxApiService: AjaxApiService) { }
+  constructor(private ajaxApiService: AjaxApiService,
+              private managerTransferState: ManageTransferStateService) { }
 
   getBoardsData(quant?: number) {
     const getBoardsStub = () => {
       return this.ajaxApiService.getBoardsData(quant);
     };
 
-    return this.ajaxApiService.getBoardsData(quant);
+    return this.managerTransferState.getRequestedData<BoardData[]>(this.boardsToken, getBoardsStub);
   }
 
   getTeamData(quant?: number) {

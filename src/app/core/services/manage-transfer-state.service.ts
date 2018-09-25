98952457e46de454215fe95815625cf287446c7b
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-25 19:10:52 +0300

    add getRequestedData() method to ManageTransferStateService

--- src/app/core/services/manage-transfer-state.service.ts
+++ src/app/core/services/manage-transfer-state.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import {PLATFORM_ID} from '@angular/core';
+import {isPlatformServer} from '@angular/common';
+import {makeStateKey, TransferState} from '@angular/platform-browser';
+import { of, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManageTransferStateService {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId,
+              private transferState: TransferState) { }
+
+  getRequestedData<T>(dataToken: string, func: () => Observable<T>) {
+    const DATA_KEY = makeStateKey<T>(dataToken);
+
+    if (this.transferState.hasKey(DATA_KEY)) {
+      const data = this.transferState.get<T>(DATA_KEY, null);
+      this.transferState.remove(DATA_KEY);
+      return of(data);
+    } else {
+      return func()
+        .pipe(
+          tap(data => {
+            if (isPlatformServer(this.platformId)) {
+              this.transferState.set(DATA_KEY, data);
+            }
+          })
+        );
+    }
+  }
 }

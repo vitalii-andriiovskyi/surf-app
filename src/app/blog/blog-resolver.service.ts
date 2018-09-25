8addfb4dfe8cca04ddb5ae26113106b40ba7c499
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-25 19:20:53 +0300

    change resolve() for using TransferState; BlogResolverService

--- src/app/blog/blog-resolver.service.ts
+++ src/app/blog/blog-resolver.service.ts
@@ -8,30 +8,21 @@ import { Router, Resolve, RouterStateSnapshot,
 import { BlogService } from './blog.service';
 import { PostItem } from './post/post';
 import { take, map } from 'rxjs/operators';
+import { ManageTransferStateService } from '../core/services/manage-transfer-state.service';
 
 @Injectable()
 export class BlogResolver implements Resolve<PostItem[]> {
   constructor(private bs: BlogService,
-              private router: Router) {}
+              private router: Router,
+              private managerTranferState: ManageTransferStateService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PostItem[]> {
 
-    return this.bs.getPosts()
-    .pipe(
-      take(1),
-      map(posts => {
-        if (posts) {
-          return posts;
-        } else { // id not found
-          this.router.navigate(['']);
-          return null;
-        }
-      })
-    );
+    return this.managerTranferState.getRequestedData<PostItem[]>('blog-posts', this.getPosts);
   }
 
   getPosts = () => {
-    this.bs.getPosts()
+    return this.bs.getPosts()
       .pipe(
         take(1),
         map(posts => {

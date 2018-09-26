1904bcaf3348236b8110d10ddb9c6d935bfdc202
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-26 20:20:08 +0300

    set production apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://www.surf-app.tech',
-  apiUrl: 'http://127.0.0.1:4040'
+  apiUrl: 'http://www.surf-app.tech',
+  // apiUrl: 'http://127.0.0.1:4040'
 };

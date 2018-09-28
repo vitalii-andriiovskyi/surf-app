9417b8e2e738888b1d7182364ae95b3d17271818
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-28 23:40:42 +0300

    remove console.log(...); SurfShellNoRenderDirective

--- src/app/shared/surf-shell-no-render.directive.ts
+++ src/app/shared/surf-shell-no-render.directive.ts
@@ -13,7 +13,6 @@ export class SurfShellNoRenderDirective implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log('log');
     if (isPlatformServer(this.platformId)) {
       this.viewContainer.clear();
     } else {

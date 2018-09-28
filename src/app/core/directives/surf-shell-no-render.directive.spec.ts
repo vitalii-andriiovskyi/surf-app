75ff7cbfc059629c797761edabf7a07f05874aac
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-28 21:42:02 +0300

    comment test for SurfShellNoRenderDirective

--- src/app/core/directives/surf-shell-no-render.directive.spec.ts
+++ src/app/core/directives/surf-shell-no-render.directive.spec.ts
@@ -1,8 +1,11 @@
 import { SurfShellNoRenderDirective } from './surf-shell-no-render.directive';
 
-describe('SurfShellNoRenderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SurfShellNoRenderDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// there's no sense to test this directive because it works just on the server
+// #TODO: find the way to test this kind of directives
+
+// describe('SurfShellNoRenderDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new SurfShellNoRenderDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });

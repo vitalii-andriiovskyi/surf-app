e4a1f951efc966b24a0b5c7b745b0c3c8c75d587
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-09-28 21:45:00 +0300

    add logic to SurfShellRenderDirective

--- src/app/core/directives/surf-shell-render.directive.ts
+++ src/app/core/directives/surf-shell-render.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, Inject, OnInit, PLATFORM_ID, TemplateRef, ViewContainerRef } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 
 @Directive({
   selector: '[surfShellRender]'
 })
-export class SurfShellRenderDirective {
+export class SurfShellRenderDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+    @Inject(PLATFORM_ID) private platformId) { }
+
+  ngOnInit() {
+    if (isPlatformServer(this.platformId)) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
 
 }
